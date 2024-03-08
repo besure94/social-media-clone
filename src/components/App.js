fa5643cede896a1192abbac056886f5016c2614f
Brian Scherner <brianscherner@gmail.com>
2024-03-08 15:40:53 -0800

    Add tabs component to App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Tabs from "./Tabs";
 
 function App() {
   return (
-    <Header/>
+    <React.Fragment>
+      <Header/>
+      <Tabs/>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Tabs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Tabs() {
+  return (
+    <React.Fragment>
+      <div id="home-tabs">
+        <h3>Home</h3>
+        <h3>Notifications</h3>
+        <h3>Messages</h3>
+      </div>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Tabs;
