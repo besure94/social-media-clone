48a8686c1ce63d6ec72aece180a3f5276c1a6ba4
Brian Scherner <brianscherner@gmail.com>
2024-03-11 13:43:51 -0700

    Adjust styling for Profile component.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,14 @@ import React from "react";
 function Profile(props) {
   return (
     <React.Fragment>
-      <div className="avatar">
-        <i class="pic">&#xf406;</i>
-        <p>{props.name}</p>
-        <div class="profile-info">
-          <p>{props.tweets}</p>
-          <p>{props.following}</p>
-          <p>{props.followers}</p>
+      <div className="profile">
+        <div className="avatar">
+          <i class="pic">&#xf406;</i>
+          <p>{props.name}</p>
+          <br/>
+            <p><span class="profile-info">{props.tweets}</span></p>
+            <p><span class="profile-info">{props.following}</span></p>
+            <p><span class="profile-info">{props.followers}</span></p>
         </div>
       </div>
     </React.Fragment>
